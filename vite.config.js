7ed74caa55a34e9327f898eeb0c319a930241bd4
jafete225 <73774270+jafete225@users.noreply.github.com>
2024-10-20 15:57:00 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -7,8 +8,13 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: './src/main.jsx',
+      input: resolve(__dirname, 'src/main.jsx'),
       external: [],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
     },
   },
 });
